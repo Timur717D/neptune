f1c24f628bb314a972ea1888199c21f2492c896d
Timur Demyanov <timur717d@gmail.com>
2026-03-29 18:59:55 +1000

    second commit

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,5 +1,5 @@
 import path from "node:path";
-import { PrismaClient } from "@/generated/prisma/client";
+import { PrismaClient } from "@prisma/client";
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
